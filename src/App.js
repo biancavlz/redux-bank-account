36e03470f4fbaf6f4c7a5b4aa2b904b0901af355
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-04 15:01:37 +0100

    Display components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import CreateCustomer from "./features/customers/CreateCustomer";
+import Customer from "./features/customers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
+    </div>
+  );
 }
 
 export default App;
