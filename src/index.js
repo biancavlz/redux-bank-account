d4894afee6f713ff2b160aa0901b7c0c6c236f92
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-04 14:59:58 +0100

    Slice reducers and apply file structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import store from "./store";
 
-import "./store";
-
+store.dispatch({ type: "account/deposit", payload: 250 });
+console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
