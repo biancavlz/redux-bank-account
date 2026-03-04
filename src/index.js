2debbad049ba5fd2a289ea8e3bab42067f165e16
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-04 15:08:36 +0100

    Connects Redux with the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
 import store from "./store";
 
 store.dispatch({ type: "account/deposit", payload: 250 });
@@ -8,6 +9,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
