daf8eec1c1b74ae42ee66531450839b1cf7deded
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-04 11:18:10 +0100

    Create a Store with basic redux

---
+++ src/store.js
@@ -0,0 +1,48 @@
+import { createStore } from "redux";
+
+// Note: 'createStore' is deprecated, it will be replaced by Redux toolkit
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, loanPurpose: "To buy a car" },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
