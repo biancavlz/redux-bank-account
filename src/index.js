183ba8a62c85f9bbbaa58f7fd17baefec37ec509
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-05 10:40:52 +0100

    add styling and small improvements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
